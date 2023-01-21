9d80d0c5038110533bb204931d388a5083d55d2a
parmxt <112687167+parmxt@users.noreply.github.com>
2023-01-21 14:07:40 +0530

    Create Q43.py

---
+++ Q43.py
@@ -0,0 +1,9 @@
+a=int(input("Enter first number : ")) 
+ b=int(input("Enter second number : ")) 
+ c=int(input("Enter third number : ")) 
+ if a<b & a<c: 
+     print(a,"is the smallest number") 
+ if b<a & b<c: 
+     print(b,"is the smallest number") 
+ if c<a & c<b: 
+     print(c,"is the smallest number")
