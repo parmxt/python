93b85f2ccf5f90be85e8b8568440cf2afb592a8e
parmxt <112687167+parmxt@users.noreply.github.com>
2023-01-21 14:05:52 +0530

    Create Q4.py

---
+++ Q4.py
@@ -0,0 +1,5 @@
+import math  
+ a=int(input("Enter the number of which you want to find Square Root : "))  
+ c=int(input("Enter the number of which you want to find Square Root : "))  
+ b=math.lcm(a, c)  
+ print(b)
